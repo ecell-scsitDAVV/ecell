74992a0546e46e5fe85156b60a1ae11407f0ec5b
Janmejay <abnjain25@gmail.com>
2025-03-19 13:35:52 +0530

    about

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -19,7 +19,7 @@ const AboutSection: React.FC = () => {
           <RevealAnimation delay={200}>
             <div className="rounded-lg overflow-hidden shadow-xl">
               <ImageWithFallback
-                src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                src="/images/E-Cell Team.jpg"
                 alt="Students collaborating"
                 className="w-full h-auto object-cover rounded-lg"
               />
